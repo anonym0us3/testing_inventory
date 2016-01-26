cd5b9115845499c7cdc2e196db124409fe7fd274
anonym0us3 <anonym0us3@users.noreply.github.com>
2016-01-25 22:38:06 -0800

    Unknown if this will also pass muster
    Adding the same #margin checking code that I used in products_controller_spec.rb file to product_spec.rb file since #margin is a model method, not a controller method.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#margin" do
+    let(:product) { FactoryGirl.create(:product) }
+
+    it "calculates margin between wholesale & retail" do
+      expect(product.margin).to eq( ( product.retail - product.wholesale ) / product.retail )
+    end
+  end
+  
 end
